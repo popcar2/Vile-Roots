0086a569a70e3e1096ef63d128bacc060f07b04d
popcar2 <ahmoudy2000@gmail.com>
2023-02-03 21:36:48 +0200

    Added player sprites

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,10 +1,10 @@
 extends CharacterBody2D
 
 @export var SPEED = 300.0
+@export var animatedSprite: AnimatedSprite2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
 
 func _physics_process(delta):
 
@@ -15,6 +15,7 @@ func _physics_process(delta):
 func move():
 	var x_direction = Input.get_axis("left", "right")
 	var y_direction = Input.get_axis("up", "down")
+	
 	if x_direction:
 		velocity.x = x_direction * SPEED
 	else:
@@ -24,3 +25,13 @@ func move():
 		velocity.y = y_direction * SPEED
 	else:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
+		
+	if x_direction or y_direction:
+		animatedSprite.play("Run")
+		if x_direction < 0:
+			animatedSprite.flip_h = true
+		else:
+			animatedSprite.flip_h = false
+	else:
+		animatedSprite.play("Idle")
+		animatedSprite.flip_h = false
