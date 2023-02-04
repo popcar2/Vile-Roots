c6beb76202b0406a697d9874f595776768ae58ef
Omar <freedadee@gmail.com>
2023-02-04 19:27:33 +0200

    Bushs & Skeletons Mobility

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -3,8 +3,9 @@ extends CharacterBody2D
 @export var health = 3
 @export var move_speed = 70
 @onready var animated_sprite: AnimatedSprite2D = $"AnimatedSprite2D"
+@onready var DisappearDelayTimer = $"DisappearDelay"
 var player: CharacterBody2D
-
+var isDead = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	player = get_tree().get_first_node_in_group("player")
@@ -13,6 +14,8 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if isDead == true:
+		return
 	var motion = position.direction_to(player.position)
 	if motion.x < 0:
 		animated_sprite.flip_h = true
@@ -25,4 +28,11 @@ func take_damage(dmg):
 	health -= dmg
 	
 	if health <= 0:
-		queue_free()
+		if isDead == false:
+			animated_sprite.play("Death")
+			DisappearDelayTimer.start()
+		isDead = true
+
+
+func _on_disappear_delay_timeout():
+	queue_free()
