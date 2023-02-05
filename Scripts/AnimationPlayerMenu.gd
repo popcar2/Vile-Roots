e6c8e48a2a5d6dfbddb7c375cb43390179f72753
Omar <freedadee@gmail.com>
2023-02-05 16:24:58 +0200

    Functional Main Menu

--- Scripts/AnimationPlayerMenu.gd
+++
@@ -1,7 +0,0 @@
-extends AnimationPlayer
-
-func _on_play_pressed():
-	play("PlayerStart")
-
-func _on_quit_pressed():
-	play("Player leave")
--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -1,18 +1,25 @@
 extends CanvasLayer
 
 @onready var mainmenu: Control = $"Main Menu"
+@export var animationplayer: AnimationPlayer
+@export var doorani: AnimatedSprite2D
+@export var timer: Timer
+@export var quittimer: Timer
 
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://GameScene.tscn")
+	animationplayer.play("PlayerStart")
+	doorani.play("OpenSesame")
+	timer.start()
 
 
 func _on_quit_pressed():
-	get_tree().quit()
-
+	animationplayer.play("Player leave")
+	doorani.play("OpenSesame")
+	quittimer.start()
 
 func _on_play_delay_timeout():
-	_on_play_pressed()
+	get_tree().change_scene_to_file("res://GameScene.tscn")
 
 
 func _on_quit_delay_timeout():
-	_on_quit_pressed()
+	get_tree().quit()
